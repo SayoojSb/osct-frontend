144f0f1a53e4744f3fcbf023d49e0cd96de97069
Sayooj S B <sayoojsb104@gmail.com>
2025-11-18 13:01:31 +0530

    updated baseurl with render url

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: "https://osct-backend-1.onrender.com/api/auth",
   withCredentials: false,
 });
 
